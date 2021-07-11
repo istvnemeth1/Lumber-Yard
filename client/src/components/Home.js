fe4a4e4e9053ed46578f867fe8be311276d436fc
Istvan Nemeth <istvnemeth1@gmail.com>
2021-07-11 18:35:03 +0100

    Setting up original navbar

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from './Navbar.js';
 
-const Navbar = props => {
+const Home = props => {
     return (
         <>
             <Navbar/>
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,36 @@
 import React from 'react'
 
 const Navbar = () => {
-    return (<h1>Navbar</h1>);
+    return (
+        <navbar class="sticky center">
+            <div class="todo">
+                <i class="bi bi-clipboard-check"></i>
+            </div>
+            <div class="home">
+                <i class="bi bi-house-door"></i>
+            </div>
+            <div class="menu">
+                <i class="bi bi-signpost-split"></i>
+            </div>
+            <div class="notification">
+                <i class="bi bi-bell"></i>
+            </div>
+            <div class="setings">
+                <i class="bi bi-person-badge"></i>
+            </div>
+            <div class="right">
+                <div class="cash">
+                    <i class="bi bi-cash"></i>
+                </div>
+                <div class="gem">
+                    <i class="bi bi-gem"></i>
+                </div>
+                <div class="coin">
+                    <i class="bi bi-piggy-bank"></i>
+                </div>
+            </div>
+        </navbar>
+    );
 };
 
 export default Navbar;
