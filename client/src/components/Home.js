06330bfcbda2afe6f852cc6762e77f8c2e08784b
Istvan Nemeth <istvnemeth1@gmail.com>
2021-07-11 18:28:13 +0100

    Bovitmeny

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,7 +5,7 @@ const Home = props => {
     return (
         <>
             <Header/>
-            <h1>Home Pityukaaaaa</h1>
+            <h1>Home Pityukaaaaa akar mi mas</h1>
             <h1>{props.data}</h1>
         </>
     );
