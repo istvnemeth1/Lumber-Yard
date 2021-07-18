96597f9107677d93ea4cfec18e5b697b47cc4959
Stefan Caky <stefan.caky777@gmail.com>
2021-07-18 22:11:11 +0100

    registration and login working on backen API

--- api/middleware/auth-user.js
+++ api/middleware/auth-user.js
@@ -18,9 +18,41 @@ const { Users } = require('../models');
  * header on the request
  */
 
-exports.authenticateUser = async(req, res, next) => {
+/***
+ * middleware function that authenticates the user credentials from the Authorization 
+ * header on the request
+ */
+ exports.authenticateUser = async (req, res, next) => {
     let message;
     const credentials = auth(req);
-
+ 
+    if (credentials) {
+        const user = await Users.findOne({
+          where: { email: credentials.name },
+        });
+        if (user) {
+          const authenticated = bcrypt.compareSync(credentials.pass, user.password);
+          if (authenticated) {
+            console.log(
+              `Authentication successful for user: ${user.userName}`
+            );
     
+            // Store the user on the Request object.
+            req.currentUser = user;
+          } else {
+            message = `Authentication failure for username: ${user.userName}`;
+          }
+        } else {
+          message = `User not found for: ${credentials.name}`;
+        }
+      } else {
+        message = "Auth header not found";
+      }
+    
+      if (message) {
+        console.log(message);
+        res.status(401).json({ message: "Access Denied" });
+      } else {
+        next();
+      }
 }
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const { authenticateUser } = require("../middleware/auth-user");
 const Users = require('../models').Users;
 const asyncHandler = require('../middleware/asyncHandler')
 
@@ -9,10 +10,28 @@ router.post('/users', asyncHandler(async (req, res) => {
       await Users.create(req.body);
       res.status(201).json({ "message": "Account successfully created!"})
   }catch(error) {
-    throw error;
+    if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
+      const errors = error.errors.map(err => err.message);
+      res.status(400).json({ errors });   
+    } else {
+      throw error;
+    }
   }
 }));
 
-router.post('/users' )
+/***
+ * return the currently authenticated user along with a 200 HTTP status code
+ */
+ router.get('/users', authenticateUser, asyncHandler(async (req, res) => {
+  const user = req.currentUser;
+  
+//  console.log(user);
+  res.status(200).json({
+    id: user.id,
+    userName: user.userName,
+    email: user.email,
+    password: user.password
+  })
+}));
 
 module.exports = router;
